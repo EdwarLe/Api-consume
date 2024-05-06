0d7756c12e7ea79bc278aef43d769eda17beb9b6
Edwar León <edwar.leon.m@gmail.com>
2024-05-06 00:34:53 -0500

    Responsive and css corrections

--- utils/printProducts.js
+++ utils/printProducts.js
@@ -4,13 +4,17 @@ function printProduct(dataProducts) {
 
     let contenedorInfo = ''
     for(let i = 0; i < dataProducts.length ; i++) {
-        if(i <= 10) {
-            contenedorInfo += `<div>
-                                        <h3>${dataProducts[i].title}</h3>
-                                        <p>Stock: ${dataProducts[i].stock}</p>
-                                        <p>Precio: $${dataProducts[i].price}</p>
-                                        <p>Descripción: ${dataProducts[i].description}</p>
-                                        <p>Categoría: ${dataProducts[i].category}</p>
+        if(i <= 11) {
+            contenedorInfo += `<div class='container-cel'>
+                                <div class='container-info'>
+                                    <h3>${dataProducts[i].title}</h3>
+                                    <div>
+                                        <span>Stock: ${dataProducts[i].stock}</span>
+                                        <span>Precio: $${dataProducts[i].price}</span>
+                                    </div>
+                                    <p>Descripción: ${dataProducts[i].description}</p>
+                                    <p>Categoría: ${dataProducts[i].category}</p>
+                                </div>
                                         <div class='img-conatiner'>
                                             <img src='${dataProducts[i].images[0]}' alt='' />
                                         </div>
